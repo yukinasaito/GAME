3954baf85a3960c05f3bde1f86374f02f5f1397c
yukinasaito <saito@oysterworld.jp>
2019-09-19 15:01:07 +0900

    09/19

--- src/main/Card2.java
+++ src/main/Card2.java
@@ -5,36 +5,19 @@ public class Card2 {
 
 	private String mark;
 	private int number;
-	
-	public	void Card(String mark, int number){
-		this.mark 	= mark;
-		this.number	= number;
-	}
-	
-		
-		
-		
-		/*public  enum mark{
-		SPADE("スペード", 4), 
-		CLOVER("クローバー", 3), 
-		DIA("ダイヤ", 2), 
-		HEART("ハート", 1), 
-		JOKER("ジョカー", 5);
-		*/
 
-		
-		public int getNumber() {
-			return number;
-		}
-		public String grtMark() {
-			return mark;
-			}
-	
-		
-		
-		
-		
-		
-	
+	public void Card(String mark, int number) {
+		this.mark = mark;
+		this.number = number;
 	}
+
+	public String getMarkString() {
+		// String[] name = {"スペード","ハート","クラブ","ダイヤ"};
+		return mark;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
 }
--- src/main/rum.java
+++ src/main/rum.java
@@ -7,21 +7,16 @@ public class rum {
 
 	public static void main(String[] args) {
 
-		Card card1 = new Card(Mark.CLOVER, 13);
-		Card card2 = new Card(Mark.CLOVER, 13);
-		System.out.println(card2.getNumber1());
-		System.out.println(card2.getMark());
+		Card card = new Card(Mark.CLOVER, 13);
+		System.out.println(card.getNumber1());
+		System.out.println(card.getMark());
 
 		Deck deck = new Deck();
 		deck.shuffle();
 
-		Card card3 = deck.draw();
-		System.out.println(card3.getNumber1());
-		System.out.println(card3.getMark());
+		Card card2 = deck.draw();
+		System.out.println(card2.getNumber1());
+		System.out.println(card.getMark());
 	}
 
-	public static int deck(String[] args) {
-		return 52;
-
-	}
 }
